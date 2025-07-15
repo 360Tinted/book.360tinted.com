35ed2dc039175f4799c3f7cf2c4f865e11a93b2d
360Tinted <contact@360tinted.com>
2025-07-15 14:44:13 -0700

    novo4
    novo4

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Importar o módulo 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'static', // Manter esta linha
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Adicionar esta seção
-    },
-  },
+  root: 'static', // Isso diz à Vite que seu index.html e assets estão na pasta 'static'
   build: {
-    outDir: '../dist', // Manter esta linha
+    outDir: '../dist', // ISSO É O NOVO: Diz à Vite para colocar a pasta 'dist' um nível acima da pasta 'root' (ou seja, na raiz do projeto)
+    emptyOutDir: true, // Garante que a pasta dist seja limpa antes de cada build
   },
+  server: {
+    host: true, // Permite acesso via IP na rede local
+    port: 5173, // Porta padrão do Vite
+  }
 })
