4ce0cba3e9a0658f78470162049ad214488a4779
360Tinted <contact@360tinted.com>
2025-07-15 16:03:58 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'; // Certifique-se de que esta linha está no topo
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // root: 'static', // Esta linha deve estar comentada ou removida
+  // Esta linha 'root: 'static'' DEVE SER REMOVIDA OU COMENTADA
   build: {
-    outDir: 'dist', // Isso fará com que a pasta 'dist' seja criada na raiz do projeto
+    outDir: 'dist', // Isso vai criar a pasta 'dist' na raiz do projeto
     emptyOutDir: true,
   },
   resolve: {
