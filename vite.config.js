39c3d81e3462e8526729c35880bb91107d13fcd3
360Tinted <contact@360tinted.com>
2025-07-15 14:59:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'; // Certifique-se de que esta linha está no topo
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: 'static', // Isso diz à Vite que seu index.html e assets estão na pasta 'static'
   build: {
-    outDir: 'dist', // MUDEI AQUI: Agora 'dist' será criado DENTRO da pasta 'static'
+    // MUDEI AQUI: outDir agora aponta para a pasta 'dist' na raiz do projeto
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
   resolve: {
